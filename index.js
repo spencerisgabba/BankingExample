0f35c92e1c67110b8378b505511cce4e76a0c147
Spencer <spennccee.schmidt0@gmail.com>
2019-11-15 14:00:21 -0500

    d

---
+++ index.js
@@ -0,0 +1,20 @@
+import lax from 'lax.js';
+import Headroom from "headroom.js";
+
+
+
+const header = document.querySelector("header");
+
+const headroom = new Headroom(header);
+headroom.init();
+
+window.onload = function () {
+    lax.setup() // init
+
+    const updateLax = () => {
+        lax.update(window.scrollY)
+        window.requestAnimationFrame(updateLax)
+    }
+
+    window.requestAnimationFrame(updateLax)
+};
